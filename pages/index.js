b8f34223b4ccee239d8981a156a6e826f38f25ad
Nicolas Marcelino Santos de Araújo <nicolas.santos@ajaxti.com.br>
2022-01-28 16:17:16 -0300

    ultimas alterações

--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,7 @@
 import { Box, Button, Text, TextField, Image } from "@skynexui/components";
+import React from "react";
+import { useRouter } from "next/router";
 import appConfig from "../config.json";
-
-function GlobalStyle() {
-  return (
-    <style global jsx>{`
-      * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        list-style: none;
-      }
-      body {
-        font-family: "Open Sans", sans-serif;
-      }
-      /* App fit Height */
-      html,
-      body,
-      #__next {
-        min-height: 100vh;
-        display: flex;
-        flex: 1;
-      }
-      #__next {
-        flex: 1;
-      }
-      #__next > * {
-        flex: 1;
-      }
-      /* ./App fit Height */
-    `}</style>
-  );
-}
 
 function Titulo(props) {
   const Tag = props.tag || "h1";
@@ -62,11 +33,12 @@ function Titulo(props) {
 // export default HomePage
 
 export default function PaginaInicial() {
-  const username = "NicolasMSA";
+  //const username = "NicolasMSA";
+  const [username, setUsername] = React.useState("NicolasMSA");
+  const roteamento = useRouter();
 
   return (
     <>
-      <GlobalStyle />
       <Box
         styleSheet={{
           display: "flex",
@@ -101,6 +73,11 @@ export default function PaginaInicial() {
           {/* Formulário */}
           <Box
             as="form"
+            onSubmit={function (infosDoEvento) {
+              infosDoEvento.preventDefault();
+              console.log("Alguém submeteu o form");
+              roteamento.push(`/chat?username=${username}`);
+            }}
             styleSheet={{
               display: "flex",
               flexDirection: "column",
@@ -123,6 +100,14 @@ export default function PaginaInicial() {
             </Text>
 
             <TextField
+              value={username}
+              onChange={function (event) {
+                console.log("usuario digitou", event.target.value);
+                //onde ta o valor?
+                const valor = event.target.value;
+                //trocar o valor da variavel atraves do react
+                setUsername(valor);
+              }}
               fullWidth
               textFieldColors={{
                 neutral: {
